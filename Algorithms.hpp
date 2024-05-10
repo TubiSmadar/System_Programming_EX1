1d367fc0b9b6eaede4b653532d1f26b0eb443614
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2024-05-10 20:10:38 +0300

    Update Algorithms.hpp
    Cleaning uneeded comments

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -9,19 +9,18 @@
 namespace ariel {
 class Algorithms {
 public:
-    static int isConnected(ariel::Graph& g);
-    static std::vector<int>* shortestPath(const ariel::Graph& g, size_t start, size_t end);
-    static std::string shortestPathToString(const std::vector<int>* shortestPath);
-    static std::vector<int> isContainsCycle(const ariel::Graph& g);
-    static bool unDirectedCycleDFS(const std::vector<std::vector<int>>& adjMatrix, size_t node, size_t parent, std::vector<bool>& visited, std::vector<int>& cycleNodes);
+    static int isConnected(ariel::Graph&);
+    static std::vector<int> shortestPath(const ariel::Graph&, size_t, size_t);
+    static std::string shortestPathToString(const std::vector<int>&);
+    static std::vector<int> isContainsCycle(const ariel::Graph&);
+    static bool unDirectedCycleDFS(const std::vector<std::vector<int>>&, size_t, size_t, std::vector<bool>&, std::vector<int>&);
     static std::string cycleToString(const std::vector<int>&);
-    static bool isSymmetric(const std::vector<std::vector<int>>& adjMatrix);
-    static bool directedCycleDFS(const std::vector<std::vector<int>>& adjMatrix, size_t node, std::vector<bool>& visited, std::vector<bool>& onStack, std::vector<int>& cycleNodes);
-
+    static bool isSymmetric(const std::vector<std::vector<int>>&);
+    static bool directedCycleDFS(const std::vector<std::vector<int>>&, size_t, std::vector<bool>&, std::vector<bool>&, std::vector<int>&);
     static std::vector<std::set<int>> isBipartite(const Graph&);
     static std::string bipartiteGraphToString(const std::vector<std::set<int>>&);
-    static void negativeCycle(const ariel::Graph& g);
-    static void printMatrix(const std::vector<std::vector<int>>& );
+    static void negativeCycle(const ariel::Graph&);
+    static void printMatrix(const std::vector<std::vector<int>>&);
 
 };
 };
