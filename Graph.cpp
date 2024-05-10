3dc685d6acd7b45914459432a956c0f332d46a4b
TubiSmadar <107445256+TubiSmadar@users.noreply.github.com>
2024-05-10 20:12:19 +0300

    Update Graph.cpp
    Added comments

--- Graph.cpp
+++ Graph.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 namespace ariel {
-    Graph::Graph() {
+    // Constructor to initialize an empty graph with zero vertices
+    Graph::Graph() : adjMatrix(vector<vector<int>>()), numVertices(0) {}
 
-    }
+    // Constructor to initialize a graph with provided adjacency matrix and number of vertices
+    Graph::Graph(const std::vector<std::vector<int>>& adjMatrix, size_t numVertices) : adjMatrix(adjMatrix), numVertices(numVertices) {}
 
+    // Method to load a graph from a given adjacency matrix
     void Graph::loadGraph(const std::vector<std::vector<int>>& matrix) {
         // Check if the matrix is square
         size_t size = matrix.size();
@@ -17,21 +20,14 @@ namespace ariel {
             }
         }
 
-        // Set the adjacency matrix
+        // Set the adjacency matrix and number of vertices
         adjMatrix = matrix;
         numVertices = size;
-
-        if (isSymmetric(matrix)) {
-            isDirected = true;
-        }
-        else {
-            isDirected = false;
-        }
-
     }
 
+    // Method to print the adjacency matrix of the graph
     void Graph::printGraph() {
-        std::cout << "Adjanceny Matrix:" <<std::endl;
+        std::cout << "Adjacency Matrix:" << std::endl;
         for (const auto &row : adjMatrix) {
             for (int val : row) {
                 std::cout << val << " ";
@@ -40,30 +36,14 @@ namespace ariel {
         }
     }
 
-    bool Graph::isSymmetric(std::vector<std::vector<int>> matrix) {
-        int rows = matrix.size();
-        if (rows == 0) {
-            return true; // Empty matrix is symmetric
-        }
-        int cols = matrix[0].size();
-
-        // Check if the matrix is square
-        if (rows != cols) {
-            return false; // Non-square matrices are not symmetric
-        }
-
-        // Check if the matrix is symmetric
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (matrix[i][j] != matrix[j][i]) {
-                    return false; // If corresponding elements are not equal, matrix is not symmetric
-                }
-            }
-        }
-        return true; // Matrix is symmetric
+    // Method to get the number of vertices in the graph
+    size_t Graph::getNumVertices() const {
+        return numVertices;
     }
-    size_t Graph::getNumVertices() const {return adjMatrix.size();}
 
-    std::vector<std::vector<int>> Graph::getAdjMatrix() const {return adjMatrix;}
+    // Method to get the adjacency matrix of the graph
+    std::vector<std::vector<int>> Graph::getAdjMatrix() const {
+        return adjMatrix;
+    }
 
 }
